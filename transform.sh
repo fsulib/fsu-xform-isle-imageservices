73dbb7f0fb73cc46f94a410d7d46268d19e13199
Malcolm Shackelford <mshackelford@fsu.edu>
2020-11-19 12:48:58 -0500

    Needed to add a step to patch the Dockerfile.

--- transform.sh
+++ transform.sh
@@ -6,7 +6,11 @@
 # isle-imageservices requires no patches, just the new delegates.rb template file
 
 [[ $CLONE_BASE ]] || CLONE_BASE=./isle-imageservices
-
+OWD="${PWD}"
 git clone https://github.com/Islandora-Collaboration-Group/isle-imageservices.git "${CLONE_BASE}"
 cp -r transformations/* "${CLONE_BASE}"
+cd "${CLONE_BASE}"
+patch < Dockerfile.patch
+find . -type f -iname \*.patch -delete
+cd "${OWD}"
 exit 0
